1f183150026865ae7545fb916816ce261000ea96
Egg-Fish <eugeniomanansala@gmail.com>
2024-10-14 23:47:26 +0800

    feat(example2): move the crate around

--- examples/example2/main.cpp
+++ examples/example2/main.cpp
@@ -9,9 +9,12 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 void main()
 {
-    Window window(600, 600, "Example 2");
+    Window window(1000, 1000, "Example 2");
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -71,6 +74,41 @@ void main()
 
     ImVec4 clearColor = ImVec4(0.2f, 0.3f, 0.3f, 1.0f);
 
+    glm::mat4 modelMatrix(1.0f);
+    shader.setUniformMatrix4fv(
+        "u_ModelMatrix",
+        1,
+        false,
+        glm::value_ptr(modelMatrix)
+    );
+
+    glm::mat4 viewMatrix = glm::lookAt(
+        glm::vec3(0.0f, 0.0f, 5.0f),
+        glm::vec3(0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)
+    );
+    shader.setUniformMatrix4fv(
+        "u_ViewMatrix",
+        1,
+        false,
+        glm::value_ptr(viewMatrix)
+    );
+
+    int width, height;
+    glfwGetWindowSize(glfwGetCurrentContext(), &width, &height);
+    glm::mat4 projectionMatrix = glm::perspective(
+        glm::radians(45.0f), 
+        (float)width/(float)height, 
+        0.1f, 
+        100.0f
+    );
+    shader.setUniformMatrix4fv(
+        "u_ProjectionMatrix",
+        1,
+        false,
+        glm::value_ptr(projectionMatrix)
+    );
+
     while (!window.shouldClose())
     {
         window.pollEvents();
@@ -80,6 +118,9 @@ void main()
         ImGui::NewFrame();
         
         static float colorIntensity = 1.0f;
+        static glm::vec3 cratePosition(0.0f);
+        static glm::vec3 crateRotation(0.0f);
+
         {
             ImGui::Begin("Hello, world!");
             ImGui::Text("This is some useful text."); 
@@ -87,12 +128,28 @@ void main()
             ImGui::SliderFloat("Color Intensity", &colorIntensity, 0.0f, 1.0f);
 
             ImGui::ColorEdit3("Background Color", (float*)&clearColor);
+            ImGui::DragFloat3("Position", glm::value_ptr(cratePosition), 0.1f, -10.0f, 10.0f);
+            ImGui::DragFloat3("Rotation", glm::value_ptr(crateRotation), 5.0f, -360.0f, 360.0f, "%.1f deg");
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
         }
+        
+        glm::mat4 modelMatrix = glm::mat4(1.0f);
+        
+        modelMatrix = glm::rotate(modelMatrix, (glm::pi<float>() / 180.0f) * crateRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+        modelMatrix = glm::rotate(modelMatrix, (glm::pi<float>() / 180.0f) * crateRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+        modelMatrix = glm::rotate(modelMatrix, (glm::pi<float>() / 180.0f) * crateRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+
+        modelMatrix = glm::translate(glm::mat4(1.0f), cratePosition) * modelMatrix;
 
         shader.setUniform1f("colorIntensity", colorIntensity);
+        shader.setUniformMatrix4fv(
+            "u_ModelMatrix",
+            1,
+            false,
+            glm::value_ptr(modelMatrix)
+        );
 
         glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
--- examples/example2/shaders/example2.vert
+++ examples/example2/shaders/example2.vert
@@ -3,12 +3,16 @@
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 uv;
 
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_ViewMatrix;
+uniform mat4 u_ProjectionMatrix;
+
 out vec3 vertexPosition;
 out vec2 texturePostion;
 
 void main()
 {
-    gl_Position = vec4(pos, 1.0);
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * vec4(pos, 1.0);
 
     vertexPosition = pos;
     texturePostion = uv;
